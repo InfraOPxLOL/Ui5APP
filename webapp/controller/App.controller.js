b573cef02cc2b9a880fbdc9bfe22b78243fec4b8
Your Name <your.email@example.com>
2026-07-06 23:25:04 +0530

    Initial commit: SAPUI5 freestyle app scaffold for Integration Suite OData integration

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller"],
+  function (Controller) {
+    "use strict";
+
+    return Controller.extend("integrationsuite.ui5app.controller.App", {
+      onInit: function () {}
+    });
+  }
+);
